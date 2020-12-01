7381942ec29fa9a0d586fec424e7a37a338dff88
Harish S.G <harishsg99@gmail.com>
2020-11-30 18:53:44 -0800

    Create config.py

---
+++ SellWhoop/config.py
@@ -0,0 +1,22 @@
+
+import os
+
+class BaseConfig:
+    TESTING = False
+    DEBUG = False
+
+class DevConfig(BaseConfig):
+    DEBUG = True
+   
+class TestConfig(BaseConfig):
+    TESTING = True
+class ProdConfig(BaseConfig):
+     pass
+
+configurations = {
+      'dev' : DevConfig,
+      'test' : TestConfig,
+      'prod' : ProdConfig,
+ }
+ 
+ 
