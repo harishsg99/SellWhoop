995e0ce236aa2915aecf683961001a5e1e6ec1e0
Harish S.G <harishsg99@gmail.com>
2020-11-30 20:18:24 -0800

    Update __init__.py

--- SellWhoop/__init__.py
+++ SellWhoop/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from SellWhoop.config import configurations
 from flask_sqlalchemy import SQLAlchemy
 from SellWhoop.extensions import db
+from SellWhoop.blueprints.products import products
 
 def create_app(environment_name ='dev'):
     app = Flask(__name__)
