fb0895b8805d7a6bdc706dda58b36d833801e1b8
Harish S.G <harishsg99@gmail.com>
2020-11-30 19:11:00 -0800

    Update __init__.py

--- SellWhoop/__init__.py
+++ SellWhoop/__init__.py
@@ -6,4 +6,5 @@ from SellWhoop.extensions import db
 def create_app(environment_name ='dev'):
     app = Flask(__name__)
     app.config.from_object(configurations[environment_name])
+    db.init_app(app)
     return app
