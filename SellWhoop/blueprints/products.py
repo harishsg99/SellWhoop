c563f91d76b55ab6ac991842fbb3c27336625493
Harish S.G <harishsg99@gmail.com>
2020-11-30 20:30:14 -0800

    Update and rename product.py to products.py

--- SellWhoop/blueprints/products.py
+++ SellWhoop/blueprints/products.py
@@ -1,9 +1,14 @@
 from flask import Blueprint
+from SellWhoop.models import Product
 
 products = Blueprints('products',__name__)
 @products.route('/')
 def index():
+  all_products = product.query.all()
   return "all the the products"
+
 @products.route('/<int:product_id>')
 def details(product_id):
-  return "product details" 
+  product = product.query.get_or_404(product_id)
+  return product.name
+
